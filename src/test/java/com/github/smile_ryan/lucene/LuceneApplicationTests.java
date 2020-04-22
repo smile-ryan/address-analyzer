ad61e75a0d04bb1fcdc912d5029195eda379fe36
ryan <chenrui@yto.net.cn>
2020-04-22 13:45:12 +0800

    initialize

---
+++ src/test/java/com/github/smile_ryan/lucene/LuceneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.smile_ryan.lucene;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuceneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
